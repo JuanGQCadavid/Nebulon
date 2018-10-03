4efb2cc9ebbc1e6d0c8cefbcb169fc011c9f3b84
rrivera2 <rrivera2@afit.edu.co>
2018-10-03 17:22:53 -0500

    Migracion
    MIgrando archivos

---
+++ web/src/forlinkneb.php
@@ -0,0 +1,47 @@
+
+<html>
+<head>
+<title>Nebulizador</title>
+<link rel="stylesheet" type="text/css" href="estilo.css">
+</head>
+
+<body bgcolor="#DAE5E4">
+
+	<div align="center">
+    <br><br><p>Link Nebulizer</p>
+    <form action="...">
+    	
+    
+    	<label for="nombre">ID: </label>
+    	<input type="text"><br>
+        
+        <label for="">Name:</label>
+    	<input type="text"><br>
+        
+        
+        <input type="submit" value="Link">
+	</form>
+    
+    <br><p>WiFi Networks</p>
+    
+    <form action="...">
+        
+        <hr/>
+        <div align="left">+ WiFiHFC0c080</div>
+        <hr/>
+        <div align="left">+ TIGO_8022</div>
+        <hr/>
+        <div align="left">- AirEafit</div>
+        <hr/>
+        
+        <label for="">Password:</label>
+    	<input type="password"><br>
+        
+        
+        <input type="submit" value="Connect">
+	</form>
+    
+    </div>
+
+</body>
+</html>
