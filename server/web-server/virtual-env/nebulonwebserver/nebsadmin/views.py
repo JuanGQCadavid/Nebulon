eea53596607ff50035ef84f614624ecff809d5f0
Hamilton <hamiltontobon77@gmail.com>
2018-11-12 14:11:35 -0500

    Login connected with the database

--- server/web-server/virtual-env/nebulonwebserver/nebsadmin/views.py
+++ server/web-server/virtual-env/nebulonwebserver/nebsadmin/views.py
@@ -1,27 +1,30 @@
 from django.shortcuts import render
-from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth import authenticate, login as django_login, logout
 
 # LogIn view
+# request is an HttpRequest object 
+#   see: https://docs.djangoproject.com/en/2.1/ref/request-response/#django.http.HttpRequest
 def login(request):
 
+    # User sent a username and a password
     if request.method == 'POST':
-        username = request.POST['username']
-        password = request.POST['password']
+        username = request.POST['userinput']
+        password = request.POST['passwordinput']
         user = authenticate(username = username, password = password)
 
         if user is not None:
             if user.is_active:
-                login(request, user)
-                return render(request, 'nebsadmin/index.html')
+                django_login(request, user)
+                return render(request, 'nebsadmin/pprincipal.html')
             else:
-                print "Your account is not active"
+                print ("Your account is not active")
                 return render(request, 'nebsadmin/login.html')
             
         else:
-            print "Wrong user or password. Try again!"
+            print("Wrong username or password. Try again!")
             return render(request, 'nebsadmin/login.html')
+
+        #return render(request, 'nebsadmin/pprincipal.html')
             
     else: # GET request
         return render(request, 'nebsadmin/login.html')
-        
-    
--- server/web-server/virtual-env/nebulonwebserver/nebulonwebserver/urls.py
+++ server/web-server/virtual-env/nebulonwebserver/nebulonwebserver/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', include('nebsadmin.urls')),
+    path('nebsadmin/', include('nebsadmin.urls')),
     path('admin/', admin.site.urls),
 ]
