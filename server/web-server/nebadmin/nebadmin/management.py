34cfb8f21f66e547dfa6c00563f7e9ffd4501d1c
Hamilton Tobon <hamiltontobon77@gmail.com>
2018-12-06 14:12:24 -0500

    Autoupdating index view with ajax

--- server/web-server/nebadmin/nebadmin/management.py
+++ server/web-server/nebadmin/nebadmin/management.py
@@ -24,7 +24,8 @@ def index():
                    'l.loan_starting_date, l.loan_ending_date '\
                    'FROM nebulon AS n '\
                    'INNER JOIN loan AS l ON n.loan_id = l.loan_id '\
-                   'INNER JOIN company AS c ON c.company_id = l.company_id')
+                   'INNER JOIN company AS c ON c.company_id = l.company_id '\
+                   'ORDER BY n.nebulon_id')
     nebulizers = cursor.fetchall()
     
     return render_template('management/index.html', nebulizers = nebulizers)
@@ -35,7 +36,9 @@ def update_liquid_level():
     db = get_db()
     cursor = db.cursor()
 
-    cursor.execute('SELECT n.nebulon_fg1level, n.nebulon_fg2level FROM nebulon AS n')
+    cursor.execute('SELECT n.nebulon_fg1name, n.nebulon_fg1level, '\
+                   'n.nebulon_fg2name, n.nebulon_fg2level '\
+                   'FROM nebulon AS n ORDER BY nebulon_id')
     levels = cursor.fetchall()
 
-    return levels
+    return str(levels)
--- server/web-server/nebadmin/nebadmin/static/js/autoupdate.js
+++ server/web-server/nebadmin/nebadmin/static/js/autoupdate.js
@@ -1,18 +1,31 @@
 function updateDoc(){
-    var xhttp = new XMLHttpRequest();
 
+    var xhttp = new XMLHttpRequest();
+    
     // Changes when the xhttp.readyState changes
     xhttp.onreadystatechange = function(){
 	if(this.readyState == 4 && this.status == 200){
 	    var levels = this.responseText;
 	    var levels_array = eval('(' + levels + ')');
 
-	    for(lvl in levels_array){
-		document.getElementById("lvl1").innetHTML =
-		    lvl['nebulon_fg1name'] + ": " + lvl['nebulon_fg1level'];
-		document.getElementById("lvl2").innetHTML =
-		    lvl['nebulon_fg2name'] + ": " + lvl['nebulon_fg2level'];
-	    }
+	    var row = 2;
+	    levels_array.forEach(function(lvl, index){
+
+		var update1 = lvl['nebulon_fg1name'] + ": " + lvl['nebulon_fg1level'] + '%';
+		var update2 = lvl['nebulon_fg2name'] + ": " + lvl['nebulon_fg2level'] + '%';
+
+		
+		document.getElementById("nebulizer-info").rows[index+2].cells[1].innerHTML = update1.toString();
+		
+		document.getElementById("nebulizer-info").rows[index+2].cells[2].innerHTML = update2.toString();
+		
+		row += 1;
+	    });
+
 	}
     };
+    xhttp.open("GET", "/management/update", true);
+    xhttp.send();
 }
+
+setInterval(updateDoc, 1500);
